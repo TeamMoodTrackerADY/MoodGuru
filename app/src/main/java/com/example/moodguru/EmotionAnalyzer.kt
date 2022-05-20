cd8fc98ed8fb734cfb6e7138f4c76b423ee78da6
Yiran Wan <ewan951218@gmail.com>
2022-05-20 10:12:14 -0700

    vader changes rating bar by jounal emo

--- app/src/main/java/com/example/moodguru/EmotionAnalyzer.kt
+++ app/src/main/java/com/example/moodguru/EmotionAnalyzer.kt
@@ -1,4 +1,22 @@
 package com.example.moodguru
 
-class EmotionalAnalyzer {
+import android.content.Context
+import android.util.Log
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+import com.example.moodguru.fragments.ComposeFragment
+
+class EmotionAnalyzer {
+    companion object{
+        fun analyze(text: String, context: Context): Float {
+            if (! Python.isStarted()) {
+                Python.start(AndroidPlatform(context))
+            }
+            val py = Python.getInstance()
+            val module = py.getModule("__init__")
+            val f = module.callAttr("analyze", text).toFloat()
+            return f
+        }
+    }
 }
--- app/src/main/java/com/example/moodguru/fragments/ComposeFragment.kt
+++ app/src/main/java/com/example/moodguru/fragments/ComposeFragment.kt
@@ -1,18 +1,29 @@
 package com.example.moodguru.fragments
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.setFragmentResultListener
 import com.bumptech.glide.Glide
+import com.example.moodguru.EmotionAnalyzer
+//import com.example.moodguru.EmotionAnalyzer
+//import com.example.moodguru.EmotionAnalyzer
 import com.example.moodguru.R
 import com.example.moodguru.SuggestionActivity
 import com.example.moodguru.parseDataModel.Emotion
+//import com.vader.sentiment.analyzer.SentimentAnalyzer
+
+//import com.vader.sentiment.analyzer.SentimentAnalyzer
 
 class ComposeFragment : Fragment() {
 
@@ -70,6 +81,23 @@ class ComposeFragment : Fragment() {
         btnContinue = view.findViewById(R.id.btnContinue)
         ratingBar = view.findViewById(R.id.rbEmotionLevel)
 
+
+//        EmotionAnalyzer.create().analyze("This is a freaking mad day. God I love it!")
+
+       etJournal.addTextChangedListener(object: TextWatcher{
+           override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+           override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+               if (!s.isNullOrEmpty()){
+                   val f = EmotionAnalyzer.analyze(s.toString(), requireContext())
+                   updateRatingBar(f)
+               }
+           }
+
+           override fun afterTextChanged(s: Editable?) {}
+
+       })
+        
         ivEmoji.setOnClickListener {
             EmotionFragment.showEmotionFragment(requireActivity().supportFragmentManager)
         }
@@ -93,5 +121,16 @@ class ComposeFragment : Fragment() {
         }
 
 
+    }
+
+    private fun updateRatingBar(rating: Float) {
+        ratingBar.rating = Math.abs(rating * 5.0).toFloat()
+        if (rating > 0.5)
+            DrawableCompat.setTint(ratingBar.progressDrawable, resources.getColor(R.color.orange_pos))
+        else if (rating < -0.5)
+            DrawableCompat.setTint(ratingBar.progressDrawable, resources.getColor(R.color.blue_neg))
+        else
+            DrawableCompat.setTint(ratingBar.progressDrawable, resources.getColor(R.color.logo_yellow))
+        Log.d(TAG, "emo = $rating")
     }
 }
